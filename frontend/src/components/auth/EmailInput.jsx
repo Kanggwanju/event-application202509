1bdc2d5a3df46bfd1a553a08276e207255d5e081
Kanggwanju <xprtktm1@gmail.com>
2025-09-03 11:21:49 +0900

    [FE] 인증번호 입력페이지로 넘어가기

--- frontend/src/components/auth/EmailInput.jsx
+++ frontend/src/components/auth/EmailInput.jsx
@@ -3,7 +3,7 @@ import styles from './SignUpForm.module.scss'
 import {AUTH_API_URL} from '../../config/host-config.js';
 import { debounce } from 'lodash';
 
-const EmailInput = () => {
+const EmailInput = ({ onSuccess }) => {
 
   const emailRef = useRef();
 
@@ -33,6 +33,8 @@ const EmailInput = () => {
       const { isDuplicate, message } = await response.json();
       if (isDuplicate) {
         setError(message);
+      } else {
+        onSuccess();
       }
     })();
 
